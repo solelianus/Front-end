1b3dfc2b1952b6e6a351ce7a7b0a6632d53d1c72
solelianus-reder <solelianus@gmail.com>
2025-11-19 17:21:10 +0330

    First commit

---
+++ JS/Intro/Intro.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+document.getElementById("demo").style.fontSize = "35px";
+
+document.getElementById("demo2").style.display = "none";
+
+document.getElementById("demo").style.display = "block";
